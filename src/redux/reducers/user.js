22c86dd94101b04c61b19881ddfd651961a11a5c
nblkhai <nblkhairunnisa13@gmail.com>
2020-04-27 18:56:28 +0700

    exercise

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 import userTypes from "./types/user";
 
 const { ON_LOGIN_SUCCESS, ON_LOGIN_FAIL, ON_LOGOUT_SUCCESS } = userTypes;
-=======
-import userTypes from "../types/user";
-
-const { ON_LOGIN_FAIL, ON_LOGIN_SUCCESS, ON_LOGOUT_SUCCESS } = userTypes;
-
->>>>>>> b3d57fa4d4c7075e9d991868afaa7eaced7bd92e
 const init_state = {
   id: 0,
   username: "",
